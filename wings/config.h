dcd5573c23691e75868cc64589a489abe772f6bf
replye <75501227+replye@users.noreply.github.com>
2021-01-21 18:50:29 +0100

    Add files via upload

---
+++ wings/config.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID       0x796d
+#define PRODUCT_ID      0x7769
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    YMDK
+#define PRODUCT         WINGS
+
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B7 }
+#define MATRIX_COL_PINS { D0, D1, D2, D3, D5, D4, D6, D7, B4, F7, F6, F5, F4, F1, F0 }
+#define UNUSED_PINS
+
+#define DIODE_DIRECTION ROW2COL
+
+#define BACKLIGHT_PIN B6
+#define BACKLIGHT_LEVELS 3
+
+#define RGB_DI_PIN E2
+#ifdef  RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 14
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 10
+#define RGBLIGHT_VAL_STEP 10
+#endif
+
+#define DEBOUNCE 5
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
---
+++ wings/wings.c
@@ -0,0 +1,1 @@
+#include "wings.h"
